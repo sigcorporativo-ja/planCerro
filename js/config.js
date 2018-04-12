c278230251dfdce97770bb13c21e810c926c2c93
fbma <fborja.manas@gmail.com>
2018-04-12 12:14:34 +0200

    Cambios en configuracion

--- js/config.js
+++ js/config.js
@@ -1,17 +1,19 @@
 var apikey="pl4n06";
 /************************************** SERVICIOS ****************************************/
-var getHermandades 	= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/hermandades/";
-var getDias 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/fechas/";
-var getRutas 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/ruta/";
-var getFechas 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/fechas/";
-var getCamino 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/camino/";
-var getPasos 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/pasos/";
-var getFechasPaso 	= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/fechas/paso/";
-var getHoras 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/horario/";
-var getGPS			= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/gps/";
-var getColor 		= "http://www.juntadeandalucia.es/justiciaeinterior/prom/rest/color/"; //NO USADO
+var getHermandades 	= "http://ws198.juntadeandalucia.es/vcab/rest/hermandades/";
+var getDias 		= "http://ws198.juntadeandalucia.es/vcab/rest/fechas/";
+var getRutas 		= "http://ws198.juntadeandalucia.es/vcab/rest/ruta/";
+var getFechas 		= "http://ws198.juntadeandalucia.es/vcab/rest/fechas/";
+var getCamino 		= "http://ws198.juntadeandalucia.es/vcab/rest/camino/";
+var getPasos 		= "http://ws198.juntadeandalucia.es/vcab/rest/pasos/";
+var getFechasPaso 	= "http://ws198.juntadeandalucia.es/vcab/rest/fechas/paso/";
+var getHoras 		= "http://ws198.juntadeandalucia.es/vcab/rest/horario/";
+var getGPS			= "http://ws198.juntadeandalucia.es/vcab/rest/gps/";
+//var getGPS			= "http://www.mocky.io/v2/56deaf14110000a303979e5c/";
+var getColor 		= "http://ws198.juntadeandalucia.es/vcab/rest/color/"; //NO USADO
 /**/
-var bboxContext = [96388,3959795,621889,4299792];
+//var bboxContext = [96388,3959795,621889,4299792];
+var bboxContext = [395827,4210307,411397,4225578];
 var zoomToPoint = 12;
 var updateGPS = 150; //en segundos
 var timeout = 15; //en segundos. Se usa para detectar si hay algún problema con los servicios no controlado
@@ -19,12 +21,12 @@ const urlPDF = "http://www.parlamentodeandalucia.es/webdinamica/portal-web-parla
 M.proxy(false);
 var attrNotShow = [ "the_geom", "geom", "geometry", "_version_", "solrid", "keywords", "equipamiento"];
 /*********************** MENSAJES DE ERROR NO CONTROLADO EN LOS SERVICIOS **********************/
-var noGPS			= "Actualmente no existen posiciones de las hermandades. Inténtelo más tarde";
-var noPosicion 		= "No existe posición para la hermandad seleccionada";
+var noGPS			= "Actualmente no existen posiciones GPS. Inténtelo más tarde";
+var noPosicion 		= "No existe posición para la selección";
 var errInesperado 	= "Ha ocurrido un error inesperado. Vuelva a ejecutar la aplicación";
 var errCode = [2];
 var errMsg = ["No es posible visualizar la ruta. El desplazamiento no se realiza en carreta"];
-var htmlAcercade	= "<img src='img/logoJunta.png'/><br>Plan Romero 2017<br>Versión 1.0.0<br><br>Junta de Andalucía<br><a href='#' onclick='javascript:openInfo();'>Consejería de Justicia e Interior</a>";
+var htmlAcercade	= "<img src='img/logoJunta.png'/><br>Plan del Cerro 2018<br>Versión 1.0.0<br><br>Junta de Andalucía<br><a href='#' onclick='javascript:openInfo();'>Consejería de Justicia e Interior</a>";
 function openInfo(){
 	cordova.InAppBrowser.open('http://www.juntadeandalucia.es/organismos/justiciaeinterior.html','_system');
 }
